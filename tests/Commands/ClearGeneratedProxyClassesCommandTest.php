6576281d4510d2bd468028551c93a5703143e8a5
yaozm <ityaozm@gmail.com>
2022-08-18 11:42:56 +0800

    Add tests

---
+++ tests/Commands/ClearGeneratedProxyClassesCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the guanguans/laravel-proxy-manager.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use Guanguans\LaravelProxyManager\Commands\ClearGeneratedProxyClassesCommand;
+use function Pest\Laravel\artisan;
+
+it('thrown no exceptions with proxy classes deleted', function () {
+    artisan(ClearGeneratedProxyClassesCommand::class)->assertSuccessful();
+});

df5be32570b0d066230943d25d60e5edbd610215
yaozm <ityaozm@gmail.com>
2022-08-23 09:59:43 +0800

    Add `extendToProxy` for `ProxyManager`

--- src/ProxyManager.php
+++ src/ProxyManager.php
@@ -327,4 +327,46 @@ class ProxyManager
             $shared
         );
     }
+
+    public function extendToAccessInterceptorScopeLocalizerProxy(string $abstract, array $prefixInterceptors = [], array $suffixInterceptors = []): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) use ($prefixInterceptors, $suffixInterceptors) {
+            return $this->createAccessInterceptorScopeLocalizerProxy($instance, $prefixInterceptors, $suffixInterceptors);
+        });
+    }
+
+    public function extendToAccessInterceptorValueHolderProxy(string $abstract, array $prefixInterceptors = [], array $suffixInterceptors = []): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) use ($prefixInterceptors, $suffixInterceptors) {
+            return $this->createAccessInterceptorValueHolderProxy($instance, $prefixInterceptors, $suffixInterceptors);
+        });
+    }
+
+    public function extendToLazyLoadingGhostFactoryProxy(string $abstract, Closure $initializer, array $proxyOptions = []): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) use ($initializer, $proxyOptions) {
+            return $this->createLazyLoadingGhostFactoryProxy(get_class($instance), $initializer, $proxyOptions);
+        });
+    }
+
+    public function extendToLazyLoadingValueHolderProxy(string $abstract, Closure $initializer, array $proxyOptions = []): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) use ($initializer, $proxyOptions) {
+            return $this->createLazyLoadingValueHolderProxy(get_class($instance), $initializer, $proxyOptions);
+        });
+    }
+
+    public function extendToNullObjectProxy(string $abstract): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) {
+            return $this->createNullObjectProxy($instance);
+        });
+    }
+
+    public function extendToRemoteObjectProxy(string $abstract, ?AdapterInterface $adapter = null): void
+    {
+        $this->container->extend($abstract, function (object $instance, Container $container) use ($adapter) {
+            return $this->createRemoteObjectProxy($instance, $adapter);
+        });
+    }
 }

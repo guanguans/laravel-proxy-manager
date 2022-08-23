f8cae1d38daef0f741e2325f44ddbbe29a58b0f5
yaozm <ityaozm@gmail.com>
2022-08-23 15:07:11 +0800

    Add Concerns

---
+++ src/Concerns/BindProxy.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This file is part of the guanguans/laravel-proxy-manager.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\LaravelProxyManager\Concerns;
+
+use Closure;
+use InvalidArgumentException;
+use ProxyManager\Factory\RemoteObject\AdapterInterface;
+use ProxyManager\Proxy\VirtualProxyInterface;
+use ReflectionClass;
+use ReflectionException;
+
+trait BindProxy
+{
+    /**
+     * bind it to a singleton proxy that will only instantiate the class when it is first used.
+     */
+    public function singletonLazyLoadingValueHolderProxy(string $className, ?Closure $concrete = null): void
+    {
+        $this->bindLazyLoadingValueHolderProxy($className, $concrete);
+    }
+
+    /**
+     * bind it to a proxy that will only instantiate the class when it is first used.
+     */
+    public function bindLazyLoadingValueHolderProxy(string $className, ?Closure $concrete = null, bool $shared = false): void
+    {
+        try {
+            $reflectionClass = new ReflectionClass($className);
+        } catch (ReflectionException $e) {
+            throw new InvalidArgumentException("Target class [$className] does not exist.");
+        }
+
+        if (! $reflectionClass->isInstantiable()) {
+            throw new InvalidArgumentException("Target [$className] is not instantiable.");
+        }
+
+        if (is_null($concrete)) {
+            $concrete = function ($container, $parameters = []) use ($className) {
+                return $this->container->build($className);
+            };
+        }
+
+        $initializer = function (?object &$wrappedObject, VirtualProxyInterface $virtualProxy, string $method, array $parameters, ?Closure &$initializer) use ($concrete) {
+            $initializer = null;
+            $wrappedObject = $concrete($this->container, []);
+
+            return true;
+        };
+
+        $this->container->bind(
+            $className,
+            function ($container, $parameters = []) use ($className, $initializer) {
+                return $this->createLazyLoadingValueHolderProxy($className, $initializer);
+            },
+            $shared
+        );
+    }
+
+    public function singletonNullObjectProxy(string $className): void
+    {
+        $this->bindNullObjectProxy($className, true);
+    }
+
+    public function bindNullObjectProxy(string $className, bool $shared = false): void
+    {
+        $this->container->bind(
+            $className,
+            function ($container, $parameters = []) use ($className) {
+                return $this->createNullObjectProxy($className);
+            },
+            $shared
+        );
+    }
+
+    public function singletonRemoteObjectProxy(string $className, ?AdapterInterface $adapter = null): void
+    {
+        $this->bindRemoteObjectProxy($className, $adapter, true);
+    }
+
+    public function bindRemoteObjectProxy(string $className, ?AdapterInterface $adapter = null, bool $shared = false): void
+    {
+        $this->container->bind(
+            $className,
+            function ($container, $parameters = []) use ($className, $adapter) {
+                return $this->createRemoteObjectProxy($className, $adapter);
+            },
+            $shared
+        );
+    }
+}
